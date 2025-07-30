e515afc102478257d6702880fe48e1ff783bc5a0
ayaz-ssvlabs <ayaz@ssvlabs.io>
2025-07-30 19:17:11 +0500

    Ayaz/fixes 2 (#16)
    * Remove unused code
    
    * Update mailbox and pingpong addresses
    
    * Update xclient, add contract bytecodes
    
    * minor fix
    
    * Add SSV label to 2PC logs

---
+++ update-genesis.sh
@@ -0,0 +1,6 @@
+BYTECODE=$(cat mailbox-bytecode | tr -d '\n')
+sed -i "s/\"alloc\": {/\"alloc\": {\n    \"aCfc748C73630F3a6E4fEEc8cF7695Eeb4836088\": {\n      \"balance\": \"0x0\",\n      \"code\": \"$BYTECODE\"\n    },/" $WORKDIR/genesis.json
+ 
+BYTECODE=$(grep "^0x" pingpong-bytecode)
+sed -i "s/\"alloc\": {/\"alloc\": {\n    \"C870117aC68BCc0D85A0C9ED1B06b29f5E834011\": {\n      \"balance\": \"0x0\",\n   \"nonce\": \"0x1\",\n  \"storage\": { \"0x000000000000000000000000000000000000000    0000000000000000000000000\": \"0x000000000000000000000000aCfc748C73630F3a6E4fEEc8cF7695Eeb4836088\" },\n \"code\": \"$BYTECODE\"\n    },/" $WORKDIR/genesis.json
+
